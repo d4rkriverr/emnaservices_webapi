f345f83712c1ade63a3f55172eaca45463f3b396
d4rkriverr <hamza.gharbi.a60@gmail.com>
2024-11-23 12:30:45 +0100

    first commit

--- go.mod
+++
@@ -1,7 +0,0 @@
-module emnaservices/webapi
-
-go 1.23.3
-
-require github.com/joho/godotenv v1.5.1
-
-require github.com/lib/pq v1.10.9
--- internal/datatype/account.go
+++
@@ -1,9 +0,0 @@
-package datatype
-
-type Account struct {
-	ID          string `json:"id"`
-	Username    string `json:"username"`
-	Password    string `json:"password"`
-	Role        string `json:"role"`
-	AccessToken string `json:"access_token"`
-}
